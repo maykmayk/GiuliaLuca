6f00e2918c85cdba3eda22bceb8ee02e7646e32d
maykmayk <m.devita@ied.edu>
2023-11-19 16:52:26 +0100

    -

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//Unique Firebase Object
+var firebaseConfig = {
+    apiKey: "AIzaSyDmRtRFgS1n-CAXnXFPnMuCCEf7YzvCsuY",
+    authDomain: "giulia-luca-32517.firebaseapp.com",
+    projectId: "giulia-luca-32517",
+    storageBucket: "giulia-luca-32517.appspot.com",
+    messagingSenderId: "787662572599",
+    appId: "1:787662572599:web:ac573ee1200f5061c383b2",
+    measurementId: "G-MEWLGZVDD8"
+};
+  
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+const db = firestore.collection("fomData");
+let submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let formValue = document.getElementById("fileInput").value;
+
+    firestore
+        .collection("fomData")
+        .get()
+        .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            const fn = doc.data().msg;
+            if (formValue === fn) {
+                console.log("Already Exists");
+            }
+            });
+        });
+    db.doc()
+        .set({
+            msg: formValue,
+        })
+        .then(() => { })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    //alert
+    alert("Your Form Has Been Submitted Successfully");
+
+    //clear form after submission
+    function clearForm() {
+        document.getElementById("myForm").reset();
+    }
+    clearForm()
+});
